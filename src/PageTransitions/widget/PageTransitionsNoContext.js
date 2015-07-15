c86a5ed49201b30814770419f97f761946837e43
bgeveritt <bgeveritt@gmail.com>
2015-07-15 16:22:42 -0400

    Addition of "No Context" widget

--- src/PageTransitions/widget/PageTransitionsNoContext.js
+++ src/PageTransitions/widget/PageTransitionsNoContext.js
@@ -2,10 +2,10 @@
 /*global mx, define, require, browser, devel, console, document, jQuery */
 /*mendix */
 /*
-    PageTransitions
+    PageTransitionsNoContext
     ========================
 
-    @file      : PageTransitions.js
+    @file      : PageTransitionsNoContext.js
     @version   : 1.0
     @author    : Bailey Everitt
     @date      : Tue, 14 Jul 2015 16:41:23 GMT
@@ -26,19 +26,18 @@ require({
     }]
 }, [
     'dojo/_base/declare', 'mxui/widget/_WidgetBase', 'dijit/_TemplatedMixin',
-    'mxui/dom', 'dojo/dom', 'dojo/query', 'dojo/dom-prop', 'dojo/dom-geometry', 'dojo/dom-class', 'dojo/dom-style', 'dojo/dom-construct', 'dojo/_base/array', 'dojo/_base/lang', 'dojo/text', 'dojo/text!PageTransitions/widget/template/PageTransitions.html'
+    'mxui/dom', 'dojo/dom', 'dojo/query', 'dojo/dom-prop', 'dojo/dom-geometry', 'dojo/dom-class', 'dojo/dom-style', 'dojo/dom-construct', 'dojo/_base/array', 'dojo/_base/lang', 'dojo/text', 'dojo/text!PageTransitions/widget/template/PageTransitionsNoContext.html'
 ], function (declare, _WidgetBase, _TemplatedMixin, dom, dojoDom, domQuery, domProp, domGeom, domClass, domStyle, domConstruct, dojoArray, lang, text, widgetTemplate) {
     'use strict';
 
     // Declare widget's prototype.
-    return declare('PageTransitions.widget.PageTransitions', [_WidgetBase, _TemplatedMixin], {
+    return declare('PageTransitions.widget.PageTransitionsNoContext', [_WidgetBase, _TemplatedMixin], {
         // _TemplatedMixin will create our dom node using this HTML template.
         templateString: widgetTemplate,
 
         // Parameters configured in the Modeler.
-        btnCaption: null,
+        clsName: null,
         direction: null,
-        microflow: null,
 
         // Internal variables. Non-primitives created in the prototype are shared between all widget instances.
         _handle: null,
@@ -53,6 +52,7 @@ require({
         // dijit._WidgetBase.postCreate is called after constructing the widget. Implement to do extra setup work.
         postCreate: function () {
             console.log(this.id + '.postCreate');
+
             this._setupEvents();
         },
 
@@ -87,13 +87,40 @@ require({
         },
 
         _setupEvents: function () {
+            
+            if (typeof window.plugins !== "undefined") {
 
-            this.domNode.innerHTML = this.btnCaption;
+                if (typeof window.plugins.nativepagetransitions !== "undefined") {
+                    this.connect(dojo.query("."+this.clsName)[0], 'click', function () {
+                        
+                        console.log('tap detected');
+                        
+                        var options = {
+                            "direction": this.direction, // 'left|right|up|down', default 'left' (which is like 'next')
+                            "duration": this.duration, // in milliseconds (ms), default 400
+                            "slowdownfactor": 2, // overlap views (higher number is more) or no overlap (1), default 4
+                            "iosdelay": 60, // ms to wait for the iOS webview to update before animation kicks in, default 60
+                            "androiddelay": 70, // same as above but for Android, default 70
+                            "winphonedelay": 200, // same as above but for Windows Phone, default 200,
+                            "fixedPixelsTop": 0, // the number of pixels of your fixed header, default 0 (iOS and Android)
+                            "fixedPixelsBottom": 0 // the number of pixels of your fixed footer (f.i. a tab bar), default 0 (iOS and Android)
+                        };
 
-            this.connect(this.domNode, 'click', function () {
-                this._execMF(this._contextObj, this.microflow);
-            });
+                        window.plugins.nativepagetransitions.slide(
+                            options,
+                            function (msg) {
+                                console.log("success: " + msg)
+                            }, // called when the animation has finished
+                            function (msg) {
+                                alert("error: " + msg)
+                            } // called in case you pass in weird values
+                        );
+                    });
 
+                }
+            } else {
+                console.log('page transition plugin not found');
+            }
         },
 
         _updateRendering: function () {
@@ -102,72 +129,6 @@ require({
 
         _resetSubscriptions: function () {
             // Release handle on previous object, if any.
-        },
-
-        _execMF: function (obj, mf, cb) {
-            if (mf) {
-                
-                //var pid = mx.ui.showProgress()
-                
-                var params = {
-                    applyto: "selection",
-                    actionname: mf,
-                    guids: []
-                };
-                if (obj) {
-                    params.guids = [obj.getGuid()];
-                }
-                mx.data.action({
-                    params: params,
-                    callback: lang.hitch(this, function (retVal) {                        
-                        if (retVal) {
-                            //mx.ui.hideProgress(pid);
-                            this._pageTransition();
-                        } else {
-                            //mx.ui.hideProgress(pid);
-                        }
-                    }),
-                    error: function (error) {
-                        // error
-                    }
-                }, this);
-
-            } else if (cb) {
-                cb();
-            }
-        },
-
-        _pageTransition: function () {
-
-            if (typeof window.plugins !== "undefined") {
-
-                if (typeof window.plugins.nativepagetransitions !== "undefined") {
-
-                    var options = {
-                        "direction": this.direction, // 'left|right|up|down', default 'left' (which is like 'next')
-                        "duration": this.duration, // in milliseconds (ms), default 400
-                        "slowdownfactor": 2, // overlap views (higher number is more) or no overlap (1), default 4
-                        "iosdelay": 60, // ms to wait for the iOS webview to update before animation kicks in, default 60
-                        "androiddelay": 70, // same as above but for Android, default 70
-                        "winphonedelay": 200, // same as above but for Windows Phone, default 200,
-                        "fixedPixelsTop": 0, // the number of pixels of your fixed header, default 0 (iOS and Android)
-                        "fixedPixelsBottom": 0 // the number of pixels of your fixed footer (f.i. a tab bar), default 0 (iOS and Android)
-                    };
-
-                    window.plugins.nativepagetransitions.slide(
-                        options,
-                        function (msg) {
-                            console.log("success: " + msg)
-                        }, // called when the animation has finished
-                        function (msg) {
-                            alert("error: " + msg)
-                        } // called in case you pass in weird values
-                    );
-                }
-
-            } else {
-                console.log('page transition plugin not found');
-            }
         }
     });
 });
